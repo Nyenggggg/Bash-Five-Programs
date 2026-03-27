7c9825ce07bfaedc6d13b5b3002ffd9ccf6b3fd0
Nyenggggg <rienniellopez321@gmail.com>
2026-03-27 16:13:50 +0800

    Add files via upload

---
+++ five.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Program to run my other four programs
+
+# Run Questionnaire
+./questionnaire.sh
+
+# Run Countdown
+./countdown.sh 3
+
+# Run Bingo
+./bingo.sh
+
+# Run fortune
+./fortune.sh
